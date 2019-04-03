3faaf52846e67f8785fac1751087b8bf9addd318
Ivanov R <you@example.com>
2019-04-03 10:33:48 +0700

    insertion_sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -2,4 +2,12 @@
 # -*- coding: utf-8 -*-
 
 def insertion_sort(a):
-    pass
+    for i in range(len(a)):
+        j = i - 1
+        key = a[i]
+        while a[j] > key and j >= 0:
+            a[j + 1] = a[j]
+            j -= 1
+        a[j + 1] = key
+    print(a)
+    return a
